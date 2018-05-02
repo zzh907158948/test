dd20c1caaeade700884cd2f6ac4bfba7bed68a2c
2doge <812921162@qq.com>
2018-05-02 19:07:57 +0800

    18/5/2作业

--- Exercise/src/Chap8/TimeTest1.java
+++ Exercise/src/Chap8/TimeTest1.java
@@ -1,4 +1,5 @@
 //应用程序类，调用Time1的方法
+//使用面向对象编程方法常常通过减少必须传递的参数来简化方法调用
 package Chap8;
 
 import javax.swing.JOptionPane;
@@ -7,6 +8,7 @@ public class TimeTest1 {
 
 	public static void main(String[] args) {
 		//声明类之后，就可以把它作为一个类型使用
+		//每次使用new运算符创建类的一个对象时，都将调用该类的构造函数以初始化新对象的实例变量
 		Time1 time = new Time1();//调用Time1的constructor
 		String output = "The initial universal time is: " + 
 		time.toUniversalString() + "\nThe initial standard time is: "
