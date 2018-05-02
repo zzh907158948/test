502eea7bf071af427ff2af30fcdf4541d6a93e8f
2doge <812921162@qq.com>
2018-05-02 17:15:26 +0800

    18/5/2作业

--- Exercise/src/Chap8/Time1.java
+++ Exercise/src/Chap8/Time1.java
@@ -10,9 +10,10 @@ public class Time1 extends Object
 	private int second;
 	
 	//constructor构造函数，初始化每个instance variable的值为0
+	//在程序创建Time1类的一个对象是，new运算符为该对象分配内存，并调用构造函数来初始化该对象
 	public Time1()
 	{
-		setTime(0,0,0);
+		setTime(0,0,0);//调用setTime方法
 	}
 	
 	//设置时间值，并把不正确的值设置为0
@@ -33,6 +34,8 @@ public class Time1 extends Object
 	
 	public String toStandardString() 
 	{
+		//将DecimalFormat类的一个对象初始化为00模式
+		//这表明一个格式化数应包含两个数字
 		DecimalFormat twoDigits = new DecimalFormat("00");
 		return ((hour == 12 || hour == 0) ? 12 : hour%12) + ":" +
 				twoDigits.format(minute) + ":" +twoDigits.format(second)
--- Exercise/src/Chap8/TimeTest1.java
+++ Exercise/src/Chap8/TimeTest1.java
@@ -6,6 +6,7 @@ import javax.swing.JOptionPane;
 public class TimeTest1 {
 
 	public static void main(String[] args) {
+		//声明类之后，就可以把它作为一个类型使用
 		Time1 time = new Time1();//调用Time1的constructor
 		String output = "The initial universal time is: " + 
 		time.toUniversalString() + "\nThe initial standard time is: "
